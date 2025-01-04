926562c7105ec81d623e54689995f862dfc83ce9
Aynlai <144365726+Aynlai@users.noreply.github.com>
2025-01-05 00:21:20 +0100

    Add Graph
    Multi agent under developing

--- shelfino_roadmap/src/rrtstar_planner.cpp
+++ shelfino_roadmap/src/rrtstar_planner.cpp
@@ -8,37 +8,47 @@
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "geometry_msgs/msg/polygon.hpp"
 #include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/point32.hpp"
 
 bool complete = false;
 
 struct Obstacle {
 	bool is_cylinder;
-	bool is_box;
 	double x,y;
 	double radius;
-	double length_x, length_y;
+	double length_vertice;
+	std::vector<std::pair<double,double>> vertices;	
 };
 
 struct Graph{
-	std::vector<Node*> nodes;
 	std::vector<Obstacle> obstacles;
-    std::vector<geometry_msgs::msg::Point> borders;	
+    std::vector<Point> borders;	
 	double width, height;
 }
+
+struct Point {
+	float x, y;
+	Point() = default;
+	Point(float _x, float _y) : x(_x), y(_y) {}
+	bool operator==(const Point &other) const
+	{ return (x == other.x&& y == other.y);}
+};
+
+
+Graph graph;
+geometry_msgs::msg::Pose start_pose;
+std::vector<Point> goal;
+
 
 class RRTStarPlanner : public rclcpp::Node {
 	public:
 		RRTStarPlanner() : Node("rrtstar_planner") {
 			// subscription_robot_position_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
 			// 	"/shelfino1/amcl_pose", qos, bind(&AStarPlanner::position_callback, this, std::placeholders::_1));
-			
-
-			// call "generate_graph" server to get graph
-			// client_ = this->create_client<path_interface::srv::GenerateGraph>("generate_graph");
 
 		}
 
-
+		// shelfino#/initialpose? 
         subscription_robot_position_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
             "/amcl_pose", qos, std::bind(&RRTStarPlanner::position_callback, this, std::placeholders::_1));
 
@@ -54,33 +64,28 @@ class RRTStarPlanner : public rclcpp::Node {
 		
 
         void getPath() {		
-
-
+			if (goal.empty()) {
+				RCLCPP_WARN(this->get_logger(), "Goal is not set. Skipping path computation.");
+				return;
+			}
 			
 			// TODO define the input parameters
 			
 			// Initialize RRT* Planner
 			double step_size = 0.1;
 			double radius = 1.0;
-			RRTstar rrtstar(&graph, step_sizde, radius);
+			RRTstar rrtstar(&graph, step_size, radius);
 
 			// Compute the path
 			std::vector<id_t> path = rrtstar.findPath(
-				start_pose.position.x, start_pose.position.y, goal.x, goal.y
+				start_pose.position.x, start_pose.position.y, goal[0].x, goal[0].y
 			);
 
 			// TODO: Call follow path client
 
 
 		}
-
-
-		// void position_callback(const geometry_msgs::msg::PoseWithCovarianceStamped msg) {
-		// 	RCLCPP_INFO(this->get_logger(), "I AM HERE!!!");
-		// 	getGraph(msg.pose.pose.position.x, msg.pose.pose.position.y);
-		// }
-
-        
+       
 
     private:
 		rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr subscription_robot_position_;
@@ -90,81 +95,70 @@ class RRTStarPlanner : public rclcpp::Node {
         rclcpp::Client<path_interface::srv::GenerateGraph>::SharedPtr client_;
 
 
-		Graph& graph;
-	    geometry_msgs::msg::Pose start_pose;
-		geometry_msgs::msg::Point goal;
-
-
-		// STARTING POINT
+		// TODO: STARTING POINT
 		void position_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
 			start_pose = msg->pose.pose;
 		}
 
 
+		// OBSTACLES
 		void obstacles_callback(const obstacles_msgs::msg::ObstacleArrayMsg::SharedPtr msg)
 		{
-			// graph.obstacles.clear();
+			graph.obstacles.clear();
 
 			for (const auto &obs : msg->obstacles) {
+				Obstacle obstacles_info;
 				vector<geometry_msgs::msg::Point32> points = obs.polygon.points;
-				vector<Point> polygon;
-
-				for (const auto& obs: points){
-					Obstacle obstacle;
-					obstacle.x = points.x;
-					obstacle.y = points.y;
-					if (obs.radius > 0.0){
-						obstacle.is_cylinder = true;
-						obstacle.radius = obs.radius;
-						graph.obstacles.push_back(obstacle);
-					}
-					else {
-						obstacle.is_box = true;
-						obstacle.length_x = ;
-						obstacle.length_y = ;
-					}
+				
+				// FOR CYLINDER
+				if (obs.radius > 0.0) { 
+					RCLCPP_INFO(this->get_logger(), "I heard a cylinder obstacle: '%f', '%f', '%f'", obs.polygon.points[0].x, obs.polygon.points[0].y, obs.radius);
+					obstacles_info.is_cylinder = true;
+					obstacles_info.x = obs.polygon.points[0].x;
+					obstacles_info.y = obs.polygon.points[0].y;
+					obstacles_info.radius = obs.radius;
+ 					graph.obstacles.push_back(obstacles_info);
 				}
 
-				// if (obs.radius > 0.0) { // cylinder
-				// 	is_cylinder = true;
-				// 	cylinder_obstacles.push_back(make_pair(obs.radius, Point(obs.polygon.points[0].x, obs.polygon.points[0].y)));
-				// 	continue;
-				// }
+				else{
+					obstacles_info.is_cylinder = false;
+					
+					for (unsigned i = 0; i < points.size(); ++i) {
+						RCLCPP_INFO(this->get_logger(), "I heard an obstacle vertex: '%f', '%f'", points[i].x, points[i].y);
+						//total_edges.push_back(Edge(Point(points[i].x, points[i].y), Point(points[(i+1)%points.size()].x, points[(i+1)%points.size()].y)));
+						obstacles_info.vertices.emplace_back(points[i].x, points[i].y);
+					}
+					graph.obstacles.push_back(obstacles_info);					
+				}
 
-				// for (unsigned i = 0; i < points.size(); ++i) {
-				// 	RCLCPP_INFO(this->get_logger(), "I heard an obstacle vertex: '%f', '%f", points[i].x, points[i].y);
-				// 	total_edges.push_back(Edge(Point(points[i].x, points[i].y), Point(points[(i+1)%points.size()].x, points[(i+1)%points.size()].y)));
-				// 	polygon.emplace_back(Point(points[i].x, points[i].y));
-				// }
-
-				// obstacle_vertices.emplace_back(polygon);
 				
 			}
 		
 		}
 
-
-		//
+		// BORDERS VERTICES
 		void borders_callback(const geometry_msgs::msg::Polygon msg)
 		{
+			graph.borders.clear();
 			vector<geometry_msgs::msg::Point32> points = msg.points;
 			for (unsigned i = 0; i < points.size(); ++i) {
-				RCLCPP_INFO(this->get_logger(), "I heard a border vertex: '%f', '%f", points[i].x, points[i].y);
-				// total_edges.push_back(Edge(Point(points[i].x, points[i].y), Point(points[(i+1)%points.size()].x, points[(i+1)%points.size()].y)));
-				// wall_vertices.push_back(Point(points[i].x, points[i].y));
+				RCLCPP_INFO(this->get_logger(), "I heard a border vertex: '%f', '%f'", points[i].x, points[i].y);
+				//total_edges.push_back(Edge(Point(points[i].x, points[i].y), Point(points[(i+1)%points.size()].x, points[(i+1)%points.size()].y)));
+				graph.borders.push_back(Point(points[i].x, points[i].y));
 			}	
-		
 		}
 
-		// GOAL
+
+
+		// GATE = GOAL
 		void gates_callback(const geometry_msgs::msg::PoseArray msg) {
-			vector<geometry_msgs::msg::Pose> poses = msg.poses;
-			for (geometry_msgs::msg::Pose& p : poses) {
-				RCLCPP_INFO(this->get_logger(), "I heard a gate vertex: '%f', '%f", p.position.x, p.position.y);
-				gates.push_back(Point(p.position.x, p.position.y));
+			//vector<geometry_msgs::msg::Pose> poses = msg.poses;
+			for (const auto& p : msg-> poses) {
+				RCLCPP_INFO(this->get_logger(), "I heard a gate vertex: '%f', '%f'", p.position.x, p.position.y);
+				goal.push_back(Point(p.position.x, p.position.y));
 			}
-		}
-};
+		}	
+}
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
